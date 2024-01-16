2e7c487d9c8ac65b18b5863dd76e5179f16d0c69
alkor63 <alkor@medi-spa.ru>
2024-01-16 23:25:46 +0300

    Добавил MapperUtil

--- src/main/java/pro/sky/registry/RegistryApplication.java
+++ src/main/java/pro/sky/registry/RegistryApplication.java
@@ -3,11 +3,13 @@ package pro.sky.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class RegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
     }
-
 }
